b07bd2fd56a3cb587931c9df3125578ece942241
Robojiang <24b953029@stu.hit.edu.cn>
2026-02-09 12:14:32 +0800

    22

--- upload_weights_DP3.sh
+++ upload_weights_DP3.sh
@@ -7,7 +7,7 @@ export ALIYUNPAN_VERBOSE=0
 export ALIYUNPAN_BIN=/mnt/afs/250010074/tao_files/aliyunpan-v0.3.7-linux-amd64/aliyunpan
 
 # 本地目录（请更改成你自己的目录）
-LOCAL_DIR="/mnt/afs/250010074/robot_manipulation/ROS2_AC-one_Play/calibration_results/D405_intrinsics.json"
+LOCAL_DIR="/mnt/afs/250010074/robot_manipulation/ROS2_AC-one_Play/policy/DP3/checkpoints/pick_place_d405-dp3-100_w_rgb_42/750.ckpt"
 # 网盘目录（请更改成你自己的目录）
 PAN_DIR="/weights/pick_place_d405/DP3"
 
