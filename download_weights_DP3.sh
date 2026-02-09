820a3b6826420b8172395ba74773fdad602899c8
tao <1481350622.qq.com>
2026-02-09 20:42:11 +0800

    downlaod

---
+++ download_weights_DP3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 是否开启调试日志
+export ALIYUNPAN_VERBOSE=0
+
+# aliyunpan程序所在的绝对路径（请更改成你自己的目录）
+export ALIYUNPAN_BIN=/home/arx/haitao_codes/aliyunpan-v0.3.7-linux-amd64/aliyunpan
+
+# 本地目录（请更改成你自己的目录）
+LOCAL_DIR="/home/arx/haitao_codes/ROS2_AC-one_Play/weights/pick_place_d405/DP3"
+# 网盘目录（请更改成你自己的目录）
+PAN_DIR="/weights/pick_place_d405/DP3/750.ckpt"
+
+# 执行下载
+"$ALIYUNPAN_BIN" download --saveto "$LOCAL_DIR" "$PAN_DIR"
