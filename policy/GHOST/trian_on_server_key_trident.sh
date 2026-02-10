49279c791f489cbe441880de9c889d1e18f475c1
Robojiang <24b953029@stu.hit.edu.cn>
2026-02-10 19:38:43 +0800

    22

--- policy/GHOST/trian_on_server_key_trident.sh
+++ policy/GHOST/trian_on_server_key_trident.sh
@@ -1,4 +1,4 @@
-cd cd /mnt/afs/250010074/robot_manipulation/ROS2_AC-one_Play/policy/GHOST
+cd /mnt/afs/250010074/robot_manipulation/ROS2_AC-one_Play/policy/GHOST
 
 # source conda.sh 让 conda activate 能用
 source /opt/miniconda3/etc/profile.d/conda.sh
