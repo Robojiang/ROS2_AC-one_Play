47e19806106dbe08d1f75d564d634e499f0e8c65
Robojiang <24b953029@stu.hit.edu.cn>
2026-03-18 22:42:38 +0800

    00

---
+++ data_progress_train.sh
@@ -0,0 +1,14 @@
+cd /mnt/afs/250010074/robot_manipulation/ROS2_AC-one_Play
+
+# source conda.sh 让 conda activate 能用
+source /opt/miniconda3/etc/profile.d/conda.sh
+
+conda activate robotwin
+
+python code_tools/convert_hdf5_to_zarr.py
+
+bash /mnt/afs/250010074/robot_manipulation/ROS2_AC-one_Play/policy/GHOST/train_on_server_trident.sh
+
+bash /mnt/afs/250010074/robot_manipulation/ROS2_AC-one_Play/policy/DP3/train_on_server.sh
+
+bash /mnt/afs/250010074/robot_manipulation/ROS2_AC-one_Play/policy/GHOST/train_on_server_key_trident.sh
