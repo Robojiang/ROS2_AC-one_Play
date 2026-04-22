6812ea82ae55b315dccfe149ed479ca79e48c680
tao <1481350622.qq.com>
2026-04-22 15:09:59 +0800

    22

--- act/collect.py
+++ act/collect.py
@@ -403,7 +403,7 @@ def parse_arguments(known=False):
 
     # 图像处理选项
     parser.add_argument('--camera_names', nargs='+', type=str,
-                        choices=['left_wrist', 'right_wrist', ],
+                        choices=['head', 'left_wrist', 'right_wrist', ],
                         default=['left_wrist', 'right_wrist'], help='camera names')
     parser.add_argument('--use_depth_image', action='store_true', help='use depth image')
 
